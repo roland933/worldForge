505d6d869a06f68136772f5b77a6458b8964b643
roland933 <mroland933@gmail.com>
2026-07-02 18:27:15 +0200

    added filter bar style

---
+++ frontend/src/components/ui/Filter/FilterBarStyle.js
@@ -0,0 +1,12 @@
+export const FilterBarStyle = {
+   default: `flex
+                items-end
+                gap-4
+                rounded-2xl
+                border
+                border-slate-700
+                bg-slate-900/50
+                p-5
+                mb-5`
+    
+}
